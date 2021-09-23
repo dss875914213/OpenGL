d32f22d2bcd841296e6345c9584b60cd20219a41
dss <dss875914213@163.com>
2021-09-24 00:01:43 +0800

    add texture

--- 03_shader/Shader.h
+++ 03_shader/Shader.h
@@ -1,13 +1,18 @@
 #pragma once
 #include "GLEW/glew.h"
+#include <string>
 
 class Shader
 {
 public:
 	Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
 	void Use();
+	void setBool(const std::string& name, bool value) const;
+	void setInt(const std::string& name, int value) const;
+	void setFloat(const std::string& name, float value) const;
 
 public:
 	GLuint Program;
+	unsigned int ID;
 };
 
---
+++ 04_texture/Shader.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "GLEW/glew.h"
+#include <string>
+
+class Shader
+{
+public:
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+	void use();
+	void setBool(const std::string& name, bool value) const;
+	void setInt(const std::string& name, int value) const;
+	void setFloat(const std::string& name, float value) const;
+public:
+	unsigned int ID:
+};
+
