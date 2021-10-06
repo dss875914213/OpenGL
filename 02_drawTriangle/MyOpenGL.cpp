5456a2949f4a5e943d4d536d897fa4e89e3c2d04
dss <dss875914213@163.com>
2021-10-06 16:16:01 +0800

    变换，旋转

--- 02_drawTriangle/MyOpenGL.cpp
+++ 02_drawTriangle/MyOpenGL.cpp
@@ -123,8 +123,8 @@ void MyOpenGL::SetTexture()
 void MyOpenGL::SetTransform()
 {
 	glm::mat4 trans = glm::mat4(1.0f);
-	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+	trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+	trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
 
 	unsigned int transformLoc = glGetUniformLocation(m_shader->ID, "transform");
 	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
