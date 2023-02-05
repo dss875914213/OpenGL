17b43d763411fb7be28db6d8de88d3cdcccbddb0
dss <dss875914213@163.com>
2023-02-05 16:18:13 +0800

    获取键盘输入，设置窗口颜色

--- TestOpenGL/main.cpp
+++ TestOpenGL/main.cpp
@@ -8,6 +8,12 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
 	glfwInit();
@@ -34,6 +40,12 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
+		processInput(window);
+
+		// ‰÷»æ÷∏¡Ó
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
